db8170f16c07c04bee49753e563b23b14294383e
William Zhao <wizhao@redhat.com>
2024-02-01 22:07:17 -0500

    Create an API module
    This creates a new "/api" go module with the existing API. This makes it
    possible for external projects/plugins to import the API structs for the
    DPU Operator without the larger set of dependencies in the project's top
    level go.mod.
    
    Signed-off-by: William Zhao <wizhao@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -30,6 +30,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	configv1 "github.com/openshift/dpu-operator/api/v1"
 	"github.com/openshift/dpu-operator/internal/controller"
@@ -67,8 +69,10 @@ func main() {
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Metrics: server.Options{
+			BindAddress: metricsAddr,
+		},
+		WebhookServer:          webhook.NewServer(webhook.Options{Port: 9443}),
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "1e46962d.openshift.io",
