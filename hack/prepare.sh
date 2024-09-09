8c4a7cba644a545a86aa493ad5fc909efd4eebec
Salvatore Daniele <sdaniele@redhat.com>
2024-09-09 13:08:44 -0400

    hack/prepare.sh: remake venv on each run
    Signed-off-by: Salvatore Daniele <sdaniele@redhat.com>

--- hack/prepare.sh
+++ hack/prepare.sh
@@ -4,6 +4,8 @@ git submodule update
 cd cluster-deployment-automation
 systemctl restart libvirtd
 
+rm -rf /tmp/ocp-venv
+
 python3.11 -m venv /tmp/ocp-venv
 source /tmp/ocp-venv/bin/activate
 sh ./dependencies.sh
