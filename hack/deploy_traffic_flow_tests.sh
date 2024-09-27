65d37635eaa4f7132283fa2016e2824605978ee4
Salvatore Daniele <sdaniele@redhat.com>
2024-09-27 11:57:56 -0400

    hack: add wa for IIC-364
    After IPU host reboot, the vsp init call fails to set an ip to
    communicate with the ACC. Restart the vsp/daemon to ensure this
    connection is up and ready to recieve cni requests.
    
    Signed-off-by: Salvatore Daniele <sdaniele@redhat.com>

--- hack/deploy_traffic_flow_tests.sh
+++ hack/deploy_traffic_flow_tests.sh
@@ -5,6 +5,13 @@ export KUBECONFIG=/root/kubeconfig.ocpcluster
 nodes=$(oc get nodes)
 export worker=$(echo "$nodes" | grep -oP '^worker-[^\s]*')
 
+
+# wa for https://issues.redhat.com/browse/IIC-364
+make undeploy
+make local-deploy
+oc create -f examples/host.yaml
+sleep 15 # Give times for Intel VSP to configure ip on <ipu-netdev>d3
+
 export KUBECONFIG=/root/kubeconfig.microshift
 nodes=$(oc get nodes)
 export acc=$(echo "$nodes" | grep -oP '^\d{3}-acc')
