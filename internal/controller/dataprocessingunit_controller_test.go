a59fc7117d7b28ca41d5d0d86efd227a0c057b87
Balazs Nemeth <bnemeth@redhat.com>
2024-10-24 15:24:23 +0200

    remove test
    Signed-off-by: Balazs Nemeth <bnemeth@redhat.com>

--- internal/controller/dataprocessingunit_controller_test.go
+++ internal/controller/dataprocessingunit_controller_test.go
@@ -17,68 +17,8 @@ limitations under the License.
 package controller
 
 import (
-	"context"
-
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	configv1 "github.com/openshift/dpu-operator/api/v1"
 )
 
 var _ = Describe("DataProcessingUnit Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
-
-		ctx := context.Background()
-
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		dataprocessingunit := &configv1.DataProcessingUnit{}
-
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind DataProcessingUnit")
-			err := k8sClient.Get(ctx, typeNamespacedName, dataprocessingunit)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &configv1.DataProcessingUnit{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
-
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &configv1.DataProcessingUnit{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Cleanup the specific resource instance DataProcessingUnit")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &DataProcessingUnitReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
-
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
-		})
-	})
 })
