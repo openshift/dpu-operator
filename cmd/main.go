973850a9e73ad547654c35604c216effac29154f
Balazs Nemeth <bnemeth@redhat.com>
2024-10-24 14:32:28 +0200

    add DPU CR
    Signed-off-by: Balazs Nemeth <bnemeth@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -110,6 +110,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ServiceFunctionChain")
 		os.Exit(1)
 	}
+	if err = (&controller.DataProcessingUnitReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DataProcessingUnit")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
