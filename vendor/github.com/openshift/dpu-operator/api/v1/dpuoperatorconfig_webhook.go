9c4e609c96217d7872156c1fa8ecdf447c4ec67a
Thomas Haller <thaller@redhat.com>
2024-11-05 13:01:49 +0100

    webhook: generate webhook using operator-sdk
    This commit is fully generated without manual changes.
    
      $ make operator-sdk && \
        ./bin/operator-sdk create webhook --group config --version v1 --kind DpuOperatorConfig --defaulting --programmatic-validation && \
        make vendor && \
        make manifests && \
        (cd api/ && go get github.com/onsi/ginkgo/v2@v2.22.0) && \
        make vendor && \
        make manifests && \
        make generate && \
        sed -i $'33i \\\tconfigv1 "github.com/openshift/dpu-operator/api/v1"' cmd/main.go && \
        sed -i 's/^- path: patches\/webhook_in_dpuoperatorconfigs.yaml/#\0/' config/crd/kustomization.yaml && \
        sed -i -e 's/var _ webhook.\(Defaulter\|Validator\)/\/\/\0/' \
            -e 's/"sigs.k8s.io\/controller-runtime\/pkg\/webhook"/\/\/\0/' \
            api/v1/dpuoperatorconfig_webhook.go \
            vendor/github.com/openshift/dpu-operator/api/v1/dpuoperatorconfig_webhook.go && \
        make bundle
    
    Note that here we also generate the defaulting/mutating webhook. We
    don't actually need that one (for now). But we generate it here, then
    the next commit will revert that change. The benefit is to have a
    explicit commit that clearly shows the mutating webhook, which later can
    be reverted if we need it.
    
    https://sdk.operatorframework.io/docs/building-operators/golang/webhook/

---
+++ vendor/github.com/openshift/dpu-operator/api/v1/dpuoperatorconfig_webhook.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	//"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// log is for logging in this package.
+var dpuoperatorconfiglog = logf.Log.WithName("dpuoperatorconfig-resource")
+
+// SetupWebhookWithManager will setup the manager to manage the webhooks
+func (r *DpuOperatorConfig) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+
+//+kubebuilder:webhook:path=/mutate-config-openshift-io-v1-dpuoperatorconfig,mutating=true,failurePolicy=fail,sideEffects=None,groups=config.openshift.io,resources=dpuoperatorconfigs,verbs=create;update,versions=v1,name=mdpuoperatorconfig.kb.io,admissionReviewVersions=v1
+
+//var _ webhook.Defaulter = &DpuOperatorConfig{}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type
+func (r *DpuOperatorConfig) Default() {
+	dpuoperatorconfiglog.Info("default", "name", r.Name)
+
+	// TODO(user): fill in your defaulting logic.
+}
+
+// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+// NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
+// Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
+//+kubebuilder:webhook:path=/validate-config-openshift-io-v1-dpuoperatorconfig,mutating=false,failurePolicy=fail,sideEffects=None,groups=config.openshift.io,resources=dpuoperatorconfigs,verbs=create;update,versions=v1,name=vdpuoperatorconfig.kb.io,admissionReviewVersions=v1
+
+//var _ webhook.Validator = &DpuOperatorConfig{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *DpuOperatorConfig) ValidateCreate() (admission.Warnings, error) {
+	dpuoperatorconfiglog.Info("validate create", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object creation.
+	return nil, nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *DpuOperatorConfig) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
+	dpuoperatorconfiglog.Info("validate update", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object update.
+	return nil, nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *DpuOperatorConfig) ValidateDelete() (admission.Warnings, error) {
+	dpuoperatorconfiglog.Info("validate delete", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object deletion.
+	return nil, nil
+}
