4f615aa13211275f0b8a14069a10a82f4edd370e
Balazs Nemeth <bnemeth@redhat.com>
2024-11-19 20:03:29 +0100

    Add P4_NAME as os env

--- cmd/intelvsp/intelvsp.go
+++ cmd/intelvsp/intelvsp.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/bn222/ipu-opi-plugins/ipu-plugin/ipuplugin/cmd"
+	"os"
 )
 
 func main() {
+	os.Setenv("P4_NAME", "fxp-net_linux-networking")
 	cmd.Execute()
 }
