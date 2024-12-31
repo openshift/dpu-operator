32063fb157ac63b1ee033bdf09ef8af9a97ddb10
Balazs Nemeth <bnemeth@redhat.com>
2024-12-31 15:22:22 +0100

    go mod vendor and tidy

---
+++ vendor/github.com/bn222/ipu-opi-plugins/ipu-plugin/pkg/ipuplugin/bridgeport.go
@@ -0,0 +1,229 @@
+// Copyright (c) 2023 Intel Corporation.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ipuplugin
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/bn222/ipu-opi-plugins/ipu-plugin/pkg/p4rtclient"
+	"github.com/bn222/ipu-opi-plugins/ipu-plugin/pkg/types"
+
+	pb "github.com/opiproject/opi-api/network/evpn-gw/v1alpha1/gen/go"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+const (
+	outerVlanId = 0 // hardcoded s-tag
+)
+
+var intfMapInit bool = false
+
+// Note: 3 reserved(last digit of interface name, for example, enp0s1f0d8, is 8) in exlude list in deviceplugin.
+var interfaces [3]string = [3]string{"6", "7", "8"}
+var intfMap map[string]bool
+
+func initMap() error {
+	if intfMapInit == false {
+		intfMap = make(map[string]bool)
+		for _, intf := range interfaces {
+			intfMap[intf] = false
+		}
+		if len(interfaces) != len(intfMap) {
+			log.Errorf("initMap setup error\n")
+			return fmt.Errorf("initMap setup error\n")
+		}
+		intfMapInit = true
+	}
+	return nil
+}
+
+// in-order(sorted by interface name->interfaces) allocation, based on available ACC interfaces(for Host VF)
+func allocateAccInterface() (error, string) {
+	var intfName string = ""
+	log.Debugf("allocateAccInterface\n")
+	if intfMapInit == false {
+		initMap()
+	}
+	for _, key := range interfaces {
+		log.Debugf("intfName->%v\n", key)
+		value, present := intfMap[key]
+		if present == true && value == false {
+			log.Debugf("Found avail Intf->%v: \n", key)
+			intfMap[key] = true
+			intfName = key
+			break
+		}
+	}
+	if intfName != "" {
+		return nil, intfName
+	}
+	log.Errorf("Interface not available\n")
+	return fmt.Errorf("Interface not available\n"), intfName
+}
+
+func freeAccInterface(intfName string) error {
+	log.Debugf("freeAccInterface\n")
+	value, present := intfMap[intfName]
+	if present == true && value == true {
+		log.Debugf("Found allocated Intf->%v: \n", intfName)
+		intfMap[intfName] = false
+		return nil
+	}
+	log.Errorf("Interface->%s not found in freeAccInterface\n", intfName)
+	return fmt.Errorf("Interface->%s not found in freeAccInterface\n", intfName)
+}
+
+// CreateBridgePort executes the creation of the port
+func (s *server) CreateBridgePort(_ context.Context, in *pb.CreateBridgePortRequest) (*pb.BridgePort, error) {
+	s.log.WithField("CreateBridgePortRequest", in).Debug("CreateBridgePort")
+	if !InitAccApfMacs {
+		log.Errorf("CreateBridgePort: AccApfs info not set, thro-> setupAccApfs")
+		return nil, fmt.Errorf("CreateBridgePort: AccApfs info not set, thro-> setupAccApfs")
+	}
+	// The assumption here is that the second octet is the VSI number.
+	// e.g.; a mac address of 00:08:00:00:03:14 the corresponding VSI is 08.
+	// VSI = 0 should be invalid and this function will return 0 when there's an error converting
+	// this octet to int value
+	macAddrSize := len(in.BridgePort.Spec.MacAddress)
+	if macAddrSize < 1 || macAddrSize > 6 {
+		// We do not have a valid mac address
+		return nil, fmt.Errorf("invalid mac address provided")
+	}
+	vfVsi := int(in.BridgePort.Spec.MacAddress[1])
+	if in.BridgePort.Spec.LogicalBridges == nil || len(in.BridgePort.Spec.LogicalBridges) < 1 {
+		return nil, fmt.Errorf("vlan id is not provided")
+	}
+	vlan := s.getFirstVlanID(in.BridgePort.Spec.LogicalBridges)
+
+	if vlan < 2 || vlan > 4094 {
+		s.log.WithField("vlan", vlan).Debug("invalid vlan")
+		return nil, fmt.Errorf("invalid vlan %d, vlan must be within 2-4094 range", vlan)
+	}
+
+	if vfVsi < 1 {
+		s.log.WithField("vfVsi", vfVsi).Debug("invalid VSI")
+		return nil, fmt.Errorf("invalid VSI:%d in given mac address, the value in 2nd octed must be > 0", vfVsi)
+	}
+
+	if isBridgePortPresent(*s, in.BridgePort.Name) {
+		return s.Ports[in.BridgePort.Name].PbBrPort, nil
+	}
+
+	CheckAndAddPeerToPeerP4Rules(s.p4rtbin)
+
+	err, intfName := allocateAccInterface()
+	if err != nil {
+		return nil, fmt.Errorf("error from allocateAccInterface->%v", err)
+	}
+
+	intIndex, err := strconv.Atoi(string(intfName))
+	if err != nil {
+		log.Errorf("error->%v converting, intfName->%v", err, intfName)
+		return nil, fmt.Errorf("error->%v converting, intfName->%v", err, intfName)
+	} else {
+		log.Infof("intIndex->%v, fullIntfName->%v", intIndex, AccIntfNames[intIndex])
+	}
+
+	if err := s.bridgeCtlr.AddPort(AccIntfNames[intIndex]); err != nil {
+		log.Errorf("failed to add port to bridge: %v, for interface->%v", err, AccIntfNames[intIndex])
+		freeAccInterface(intfName)
+		return nil, fmt.Errorf("failed to add port to bridge: %v, for interface->%v", err, AccIntfNames[intIndex])
+	}
+
+	// Add FXP rules
+	log.Infof("AddHostVfP4Rules, path->%s, 1->%v, 2->%v", s.p4rtbin, in.BridgePort.Spec.MacAddress, AccApfMacList[intIndex])
+	p4rtclient.AddHostVfP4Rules(s.p4rtbin, in.BridgePort.Spec.MacAddress, AccApfMacList[intIndex])
+
+	resp := proto.Clone(in.BridgePort).(*pb.BridgePort)
+	resp.Status = &pb.BridgePortStatus{OperStatus: pb.BPOperStatus_BP_OPER_STATUS_UP}
+	pbBridgePortInfo := &types.BridgePortInfo{PbBrPort: resp, PortInterface: intfName}
+	s.Ports[in.BridgePort.Name] = pbBridgePortInfo
+	return resp, nil
+}
+
+// isBridgePortPresent checks if the bridge port is present
+func isBridgePortPresent(srv server, brPortName string) bool {
+	_, ok := srv.Ports[brPortName]
+	return ok
+}
+
+// DeleteBridgePort deletes a port
+func (s *server) DeleteBridgePort(_ context.Context, in *pb.DeleteBridgePortRequest) (*emptypb.Empty, error) {
+	s.log.WithField("DeleteBridgePortRequest", in).Info("DeleteBridgePort")
+
+	if !InitAccApfMacs {
+		log.Errorf("DeleteBridgePort: AccApfs info not set, thro-> setupAccApfs")
+		return nil, fmt.Errorf("DeleteBridgePort: AccApfs info not set, thro-> setupAccApfs")
+	}
+	var portInfo *pb.BridgePort
+	brPortInfo, ok := s.Ports[in.Name]
+	if !ok {
+		s.log.WithField("interface name", in.Name).Info("port info is not found")
+		// in such case avoid delete call loop from CNI Agent which otherwise will repeatedly call DeleteBridgePort as retry
+		return &emptypb.Empty{}, nil
+	}
+	portInfo = brPortInfo.PbBrPort
+
+	intIndex, err := strconv.Atoi(string(brPortInfo.PortInterface))
+	if err != nil {
+		log.Errorf("error->%v converting, intfName->%v", err, brPortInfo.PortInterface)
+		return nil, fmt.Errorf("error->%v converting, intfName->%v", err, brPortInfo.PortInterface)
+	} else {
+		log.Infof("intIndex->%v, fullIntfName->%v", intIndex, AccIntfNames[intIndex])
+	}
+
+	if err := s.bridgeCtlr.DeletePort(AccIntfNames[intIndex]); err != nil {
+		log.Errorf("unable to delete port from bridge: %v, for interface->%v", err, AccIntfNames[intIndex])
+		return nil, fmt.Errorf("unable to delete port from bridge: %v, for interface->%v", err, AccIntfNames[intIndex])
+	}
+	freeAccInterface(brPortInfo.PortInterface)
+	// Delete FXP rules
+	log.Infof("DeleteHostVfP4Rules, path->%s, 1->%v, 2->%v", s.p4rtbin, portInfo.Spec.MacAddress, AccApfMacList[intIndex])
+	p4rtclient.DeleteHostVfP4Rules(s.p4rtbin, portInfo.Spec.MacAddress, AccApfMacList[intIndex])
+
+	delete(s.Ports, in.Name)
+	return &emptypb.Empty{}, nil
+}
+
+// UpdateBridgePort updates an Nvme Subsystem
+func (s *server) UpdateBridgePort(_ context.Context, in *pb.UpdateBridgePortRequest) (*pb.BridgePort, error) {
+	s.log.WithField("UpdateBridgePortRequest", in).Info("UpdateBridgePort")
+	return &pb.BridgePort{}, nil
+}
+
+// GetBridgePort gets an BridgePort
+func (s *server) GetBridgePort(_ context.Context, in *pb.GetBridgePortRequest) (*pb.BridgePort, error) {
+	s.log.WithField("GetBridgePortRequest", in).Info("GetBridgePort")
+	return &pb.BridgePort{Name: in.Name, Spec: &pb.BridgePortSpec{}}, nil
+}
+
+// GetBridgePort gets an BridgePort
+func (s *server) ListBridgePorts(_ context.Context, in *pb.ListBridgePortsRequest) (*pb.ListBridgePortsResponse, error) {
+	s.log.WithField("ListBridgePortsRequest", in).Info("ListBridgePorts")
+	return &pb.ListBridgePortsResponse{}, nil
+}
+
+func (s *server) getFirstVlanID(bridges []string) int {
+	vlanId, err := strconv.Atoi(bridges[0])
+	if err != nil {
+		s.log.Errorf("unable to parse vlan ID %s. conversion error %s", bridges[0], err)
+		return 0
+	}
+	return vlanId
+}
---
+++ vendor/github.com/bn222/ipu-opi-plugins/ipu-plugin/pkg/ipuplugin/deviceplugin.go
@@ -0,0 +1,267 @@
+package ipuplugin
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/bn222/ipu-opi-plugins/ipu-plugin/pkg/types"
+	log "github.com/sirupsen/logrus"
+
+	pb "github.com/openshift/dpu-operator/dpu-api/gen"
+	pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
+)
+
+type DevicePluginService struct {
+	pb.UnimplementedDeviceServiceServer
+	mode string
+}
+
+var (
+	//TODO: Use (GetFilteredPfs), to find interface names to be excluded.
+	//excluding d3(host-acc), reserving D4-D5(QSPF ports) D6-D8(for max 3 host VFs), D9-D10(for single NF)
+	exclude = []string{"enp0s1f0", "enp0s1f0d1", "enp0s1f0d2", "enp0s1f0d3", "enp0s1f0d4", "enp0s1f0d5", "enp0s1f0d6",
+		"enp0s1f0d7", "enp0s1f0d8", "enp0s1f0d9", "enp0s1f0d10"}
+	sysClassNet      = "/sys/class/net"
+	sysBusPciDevices = "/sys/bus/pci/devices"
+	deviceCode       = "0x1452"
+	deviceCodeVf     = "0x145c"
+	intelVendor      = "0x8086"
+	maxVfsSupported  = 64
+)
+
+func NewDevicePluginService(mode string) *DevicePluginService {
+	return &DevicePluginService{mode: mode}
+}
+
+func (s *DevicePluginService) GetDevices(context.Context, *pb.Empty) (*pb.DeviceListResponse, error) {
+
+	devices, err := discoverHostDevices(s.mode)
+	if err != nil {
+		return &pb.DeviceListResponse{}, err
+	}
+
+	response := &pb.DeviceListResponse{
+		Devices: devices,
+	}
+
+	log.Debugf("GetDevices, response->%v\n", response)
+	return response, nil
+}
+
+// Note: This function below was taken from open-source sriov plugin, which
+// is also under the same apache license, with few additional changes.
+// Returns a List containing PCI addr for all VF discovered in a given PF
+func GetVFList(pf string) (vfList []string, err error) {
+	vfList = make([]string, 0)
+	pfDir := filepath.Join(sysBusPciDevices, pf)
+	_, err = os.Lstat(pfDir)
+	if err != nil {
+		log.Errorf("error. Could not get PF directory information for device: %s, Err: %v", pf, err)
+		err = fmt.Errorf("error. Could not get PF directory information for device: %s, Err: %v", pf, err)
+		return
+	}
+
+	vfDirs, err := filepath.Glob(filepath.Join(pfDir, "virtfn*"))
+
+	if err != nil {
+		log.Errorf("error reading VF directories %v", err)
+		err = fmt.Errorf("error reading VF directories %v", err)
+		return
+	}
+
+	// Read all VF directory and get add VF PCI addr to the vfList
+	for _, dir := range vfDirs {
+		dirInfo, err := os.Lstat(dir)
+		if err == nil && (dirInfo.Mode()&os.ModeSymlink != 0) {
+			linkName, err := filepath.EvalSymlinks(dir)
+			if err == nil {
+				vfLink := filepath.Base(linkName)
+				vfList = append(vfList, vfLink)
+			}
+		}
+	}
+	return
+}
+
+func GetVfDeviceCount(pciAddr string) (int, error) {
+	vfList, err := GetVFList(pciAddr)
+	if err != nil {
+		log.Errorf("Error->%v, from GetVFList", err)
+		return 0, fmt.Errorf("Error->%v, from GetVFList", err)
+	}
+	length := len(vfList)
+	log.Infof("GetVFList count->%v", length)
+	return length, nil
+}
+
+// Recommended to wait upto 2 seconds, for the max VFs(64) currently supported.
+func pollToCheckVfDevices(pciAddr string, count int) error {
+	var err error
+	cnt := 0
+	ticker := time.NewTicker(time.Second / 3)
+	done := make(chan bool, 1)
+
+	go func() {
+		for _ = range ticker.C {
+			cnt, err = GetVfDeviceCount(pciAddr)
+			if cnt == count && err == nil {
+				ticker.Stop()
+				break
+			}
+		}
+		done <- true
+	}()
+
+	timer := time.NewTimer(time.Second * 2)
+	select {
+	case <-done:
+		log.Debugf("ticker Done")
+		timer.Stop()
+	case <-timer.C:
+		log.Debugf("timer.C Done")
+		ticker.Stop()
+	}
+
+	if cnt == count && err == nil {
+		log.Debugf("pollToCheckVfDevices: match expected count, %v\n", cnt)
+		return nil
+	}
+	log.Debugf("pollToCheckVfDevices: Timed out:attempt->%v, result->%v, err->%v\n", count, cnt, err)
+	return fmt.Errorf("pollToCheckVfDevices: Timed out:attempt->%v, result->%v, err->%v\n", count, cnt, err)
+}
+
+func SetNumSriovVfs(mode string, pciAddr string, vfCnt int32) error {
+
+	//Note: Upto 64 VFs have been validated.
+	if vfCnt <= 0 || vfCnt > int32(maxVfsSupported) {
+		return fmt.Errorf("SetNumSriovVfs(): Invalid/unsupported, vfCnt->%v \n", vfCnt)
+	}
+
+	pathToNumVfsFile := filepath.Join(sysBusPciDevices, pciAddr, "sriov_numvfs")
+
+	//Need to first write 0 for num of VFs, before updating it.
+	err := os.WriteFile(pathToNumVfsFile, []byte("0"), 0644)
+	if err != nil {
+		return fmt.Errorf("SetNumSriovVfs(): reset fail %s: %v", pathToNumVfsFile, err)
+	}
+	zeroVfs := 0
+	err = pollToCheckVfDevices(pciAddr, zeroVfs)
+	if err != nil {
+		return fmt.Errorf("cli-client query failed count->%v, err->%v\n", zeroVfs, err)
+	}
+
+	// Note: Post-writing, it can take some time for the VFs to be created.
+	err = os.WriteFile(pathToNumVfsFile, []byte(strconv.Itoa(int(vfCnt))), 0644)
+	if err != nil {
+		return fmt.Errorf("SetNumSriovVfs():error in updating %s: %v", pathToNumVfsFile, err)
+	}
+
+	err = pollToCheckVfDevices(pciAddr, int(vfCnt))
+	if err != nil {
+		return fmt.Errorf("cli-client query failed count->%v, err->%v\n", vfCnt, err)
+	}
+
+	log.Debugf("SetNumSriovVfs(): updated file->%s, sriov_numvfs to %v\n", pathToNumVfsFile, vfCnt)
+
+	return nil
+}
+
+func SetNumVfs(mode string, numVfs int32) (int32, error) {
+	deviceVfsSet := false
+
+	if mode != types.HostMode {
+		return 0, fmt.Errorf("setNumVfs(): only supported on host: mode %s\n", mode)
+	}
+
+	log.Debugf("setNumVfs(): requested num of VFs->%v\n", numVfs)
+
+	files, err := os.ReadDir(sysBusPciDevices)
+	if err != nil {
+		return 0, fmt.Errorf("setNumVfs(): error-> %v\n", err)
+	}
+
+	for _, file := range files {
+		deviceByte, err := os.ReadFile(filepath.Join(sysBusPciDevices, file.Name(), "device"))
+		if err != nil {
+			log.Errorf("Error reading PCIe deviceID: %s\n", err)
+			continue
+		}
+
+		vendorByte, err := os.ReadFile(filepath.Join(sysBusPciDevices, file.Name(), "vendor"))
+		if err != nil {
+			log.Errorf("Error reading VendorID: %s\n", err)
+			continue
+		}
+
+		deviceId := strings.TrimSpace(string(deviceByte))
+		vendorId := strings.TrimSpace(string(vendorByte))
+
+		if deviceId == deviceCode && vendorId == intelVendor {
+			err = SetNumSriovVfs(mode, file.Name(), numVfs)
+			if err != nil {
+				return 0, fmt.Errorf("setNumVfs(): error from SetSriovNumVfs-> %v", err)
+			}
+			deviceVfsSet = true
+			break
+		}
+	}
+	if deviceVfsSet == true {
+		return numVfs, nil
+	} else {
+		return 0, fmt.Errorf("setNumVfs(): unable to set VFs for device->%s", deviceCode)
+	}
+}
+
+func (s *DevicePluginService) SetNumVfs(ctx context.Context, vfCountReq *pb.VfCount) (*pb.VfCount, error) {
+	var res *pb.VfCount
+	numVfs, err := SetNumVfs(types.HostMode, vfCountReq.VfCnt)
+
+	log.Debugf("setNumVfs(): requested VFs->%v, allocated VFs->%v, err->%v\n", vfCountReq.VfCnt, numVfs, err)
+	if err != nil {
+		res = &pb.VfCount{VfCnt: 0}
+	} else {
+		res = &pb.VfCount{VfCnt: numVfs}
+	}
+
+	log.Debugf("SetNumVfs res->%v\n", res)
+	return res, err
+}
+
+func discoverHostDevices(mode string) (map[string]*pb.Device, error) {
+
+	devices := make(map[string]*pb.Device)
+
+	files, err := os.ReadDir(sysClassNet)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return make(map[string]*pb.Device), nil
+		}
+	}
+
+	for _, file := range files {
+		deviceCodeByte, err := os.ReadFile(filepath.Join(sysClassNet, file.Name(), "device/device"))
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
+
+		device_code := strings.TrimSpace(string(deviceCodeByte))
+		if mode == types.IpuMode {
+			if device_code == deviceCode {
+				if !slices.Contains(exclude, file.Name()) {
+					devices[file.Name()] = &pb.Device{ID: file.Name(), Health: pluginapi.Healthy}
+				}
+			}
+		} else if mode == types.HostMode {
+			if device_code == deviceCodeVf {
+				devices[file.Name()] = &pb.Device{ID: file.Name(), Health: pluginapi.Healthy}
+			}
+		}
+	}
+	return devices, nil
+}
