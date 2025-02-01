ead58a50c3bc8cfe5ca67acd04387a78eeb2f96a
Balazs Nemeth <bnemeth@redhat.com>
2025-02-01 09:47:47 +0100

    Only init venv if it makes sense
    Signed-off-by: Balazs Nemeth <bnemeth@redhat.com>

--- hack/prepare-venv.sh
+++ hack/prepare-venv.sh
@@ -2,18 +2,30 @@
 
 set -e
 
-cd cluster-deployment-automation
-rm -rf /tmp/cda-venv
-python3.11 -m venv /tmp/cda-venv
-source /tmp/cda-venv/bin/activate
-sh ./dependencies.sh
-deactivate
-cd -
+setup_venv() {
+    local submodule_dir=$1
+    local venv_path=$2
+    local setup_script=$3
+    local sha_file=$venv_path/.submodule_sha
 
-cd ocp-traffic-flow-tests
-rm -rf /tmp/tft-venv
-python3.11 -m venv /tmp/tft-venv
-source /tmp/tft-venv/bin/activate
-pip install -r requirements.txt
-deactivate
-cd -
+    cd $submodule_dir
+
+    current_sha=$(git rev-parse HEAD)
+
+    if [[ -d $venv_path && -f $sha_file && $(cat $sha_file) == "$current_sha" ]]; then
+        echo "Virtual environment for $submodule_dir is up to date. Skipping setup."
+    else
+        echo "Setting up virtual environment for $submodule_dir..."
+        rm -rf $venv_path
+        python3.11 -m venv $venv_path
+        source $venv_path/bin/activate
+        eval $setup_script
+        echo $current_sha > $sha_file
+        deactivate
+    fi
+
+    cd -
+}
+
+setup_venv cluster-deployment-automation /tmp/cda-venv "sh ./dependencies.sh"
+setup_venv ocp-traffic-flow-tests /tmp/tft-venv "pip install -r requirements.txt"
