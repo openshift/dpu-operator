53c162fcfbcad5541e402de2f819d49de70fde01
Thomas Haller <thaller@redhat.com>
2025-02-18 14:35:06 +0100

    webhook/e2e_test: add test for validating webhook

--- e2e_test/e2e_test.go
+++ e2e_test/e2e_test.go
@@ -51,8 +51,16 @@ var _ = g.AfterSuite(func() {
 
 var _ = g.Describe("Dpu side", g.Ordered, func() {
 	var (
-		dpuSideClient client.Client
+		dpuSideClient                      client.Client
+		restoreDpuOperatorConfigInAfterAll func()
 	)
+
+	g.AfterAll(func() {
+		if restoreDpuOperatorConfigInAfterAll != nil {
+			restoreDpuOperatorConfigInAfterAll()
+			restoreDpuOperatorConfigInAfterAll = nil
+		}
+	})
 
 	g.BeforeEach(func() {
 		cluster := testutils.CdaCluster{
@@ -68,6 +76,150 @@ var _ = g.Describe("Dpu side", g.Ordered, func() {
 	})
 
 	g.AfterEach(func() {
+	})
+
+	g.Context("when Validating Webhook for DpuOperatorConfig is running", func() {
+
+		skipTests := false
+
+		g.It("should handle precondition of existing DpuOperatorConfig to prepare test environment", func() {
+			dpuOperatorConfig0 := &configv1.DpuOperatorConfig{}
+			err := dpuSideClient.Get(context.TODO(), configv1.DpuOperatorConfigNamespacedName, dpuOperatorConfig0)
+			if err == nil {
+				/* This test runs against a real cluster. Such a cluster commonly already
+				 * has a DpuOperatorConfig. This test wants to create different DpuOperatorConfig
+				 * to check whether the validating webhook is running. For that, it first needs
+				 * to delete (and later restore, see "restoreDpuOperatorConfigInAfterAll") the
+				 * DpuOperatorConfig.
+				 *
+				 * However, the followup test for SFC has a problem with that, and will fail
+				 * when recreating the DpuOperatorConfig.
+				 *
+				 * As temporary workaround, skip.
+				 */
+				skipTests = true
+				g.Skip("Skipping test as external DpuOperatorConfig already exists")
+			}
+			if err == nil {
+				spec := dpuOperatorConfig0.Spec
+
+				restoreDpuOperatorConfigInAfterAll = func() {
+					/* We are about to delete the DpuOperatorConfig so we can run our test.
+					 * At the end, we will create a new one via this function. */
+					config := configv1.DpuOperatorConfig{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: vars.Namespace,
+							Name:      vars.DpuOperatorConfigName,
+						},
+						Spec: spec,
+					}
+					err := dpuSideClient.Create(context.TODO(), &config)
+					Expect(err).NotTo(HaveOccurred())
+					config = configv1.DpuOperatorConfig{}
+					Eventually(func() error {
+						return dpuSideClient.Get(context.TODO(), configv1.DpuOperatorConfigNamespacedName, &config)
+					}, testutils.TestAPITimeout, testutils.TestRetryInterval).Should(Succeed())
+				}
+
+				err = dpuSideClient.Delete(context.TODO(), dpuOperatorConfig0)
+				Expect(err).NotTo(HaveOccurred())
+			}
+
+			Eventually(func() error {
+				config := configv1.DpuOperatorConfig{}
+				return dpuSideClient.Get(context.TODO(), configv1.DpuOperatorConfigNamespacedName, &config)
+			}, testutils.TestAPITimeout, testutils.TestRetryInterval).ShouldNot(Succeed())
+		})
+
+		g.It("should fail to add DpuOperatorConfig with invalid Name", func() {
+			if skipTests {
+				g.Skip("Skipping test as external DpuOperatorConfig already exists")
+			}
+			configInvalidName := configv1.DpuOperatorConfig{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: vars.Namespace,
+					Name:      "invalidname",
+				},
+				Spec: configv1.DpuOperatorConfigSpec{
+					Mode:     "host",
+					LogLevel: 2,
+				},
+			}
+			err := dpuSideClient.Create(context.TODO(), &configInvalidName)
+			Expect(err).To(MatchError(ContainSubstring("DpuOperatorConfig must have standard name")))
+		})
+
+		g.It("should fail to add DpuOperatorConfig with invalid Mode", func() {
+			if skipTests {
+				g.Skip("Skipping test as external DpuOperatorConfig already exists")
+			}
+			configInvalidMode := configv1.DpuOperatorConfig{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: vars.Namespace,
+					Name:      vars.DpuOperatorConfigName,
+				},
+				Spec: configv1.DpuOperatorConfigSpec{
+					Mode:     "invalidmode",
+					LogLevel: 2,
+				},
+			}
+			err := dpuSideClient.Create(context.TODO(), &configInvalidMode)
+			Expect(err).To(MatchError(ContainSubstring("Invalid mode")))
+		})
+
+		createDpuOperatorConfig := func() error {
+			configGood := configv1.DpuOperatorConfig{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: vars.Namespace,
+					Name:      vars.DpuOperatorConfigName,
+				},
+				Spec: configv1.DpuOperatorConfigSpec{
+					Mode:     "auto",
+					LogLevel: 2,
+				},
+			}
+
+			return dpuSideClient.Create(context.TODO(), &configGood)
+		}
+
+		deleteDpuOperatorConfig := func() error {
+			configToDelete := configv1.DpuOperatorConfig{}
+			err := dpuSideClient.Get(context.TODO(), configv1.DpuOperatorConfigNamespacedName, &configToDelete)
+			if err != nil {
+				return err
+			}
+			return dpuSideClient.Delete(context.TODO(), &configToDelete)
+		}
+
+		g.It("should succeed to add a standard DpuOperatorConfig", func() {
+			if skipTests {
+				g.Skip("Skipping test as external DpuOperatorConfig already exists")
+			}
+			Expect(createDpuOperatorConfig()).NotTo(HaveOccurred())
+			Expect(deleteDpuOperatorConfig()).NotTo(HaveOccurred())
+		})
+
+		g.It("Should handle update to DpuOperatorConfig", func() {
+			if skipTests {
+				g.Skip("Skipping test as external DpuOperatorConfig already exists")
+			}
+
+			Expect(createDpuOperatorConfig()).NotTo(HaveOccurred())
+
+			configToUpdate := configv1.DpuOperatorConfig{}
+			err := dpuSideClient.Get(context.TODO(), configv1.DpuOperatorConfigNamespacedName, &configToUpdate)
+			Expect(err).NotTo(HaveOccurred())
+
+			configToUpdate.Spec.Mode = "invalidmode"
+			err = dpuSideClient.Update(context.TODO(), &configToUpdate)
+			Expect(err).To(MatchError(ContainSubstring("Invalid mode")))
+
+			configToUpdate.Spec.Mode = "dpu"
+			err = dpuSideClient.Update(context.TODO(), &configToUpdate)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(deleteDpuOperatorConfig()).NotTo(HaveOccurred())
+		})
 	})
 
 	g.Context("ServiceFunctionChain", func() {
