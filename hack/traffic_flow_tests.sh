b6b6fdcfc0bb005a84ba2e310f937e953e0ff240
Thomas Haller <thaller@redhat.com>
2025-04-11 08:36:55 +0200

    tft: use "--check" option to validate result
    The new "--check" option tells "tft.py" to basically do a
    "print_results.py" call right after. It combines those two
    steps in one.
    
    Use it.

--- hack/traffic_flow_tests.sh
+++ hack/traffic_flow_tests.sh
@@ -30,6 +30,4 @@ envsubst < ../hack/cluster-configs/ocp-tft-config.yaml > $temp_file
 export TFT_MANIFESTS_OVERRIDES=../hack/cluster-configs/traffic_flow_manifests
 OUTPUT_BASE="./ft-logs/result-$(date '+%Y%m%d-%H%M%S.%4N-')"
 
-./tft.py -v debug --output-base "$OUTPUT_BASE" "$temp_file"
-
-./print_results.py "$OUTPUT_BASE"*.json
+./tft.py --check -v debug --output-base "$OUTPUT_BASE" "$temp_file"
