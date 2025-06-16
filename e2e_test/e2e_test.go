fb2be2c920f494d59012bb6921c5cfc6ab827802
Thomas Haller <thaller@redhat.com>
2025-06-16 11:21:04 +0200

    e2e-test: add test creating multiple SFCs in parallel
    Create a bunch of SFCs. As the "openshift.io/dpu" resource is limited,
    they probably cannot start all right away. However, we are also deleting
    the SFCs after a short while, so eventually all SFCs should have their
    pods running and quit.
    
    https://issues.redhat.com/browse/IIC-685

--- e2e_test/e2e_test.go
+++ e2e_test/e2e_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/openshift/dpu-operator/internal/scheme"
 	"github.com/openshift/dpu-operator/internal/testutils"
 	"github.com/openshift/dpu-operator/pkgs/vars"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -38,11 +39,61 @@ var (
 
 const (
 	// TODO: reduce to 2 seconds
-	timeout  = 1 * time.Minute
-	interval = 1 * time.Second
-	nfName   = "test-nf"
-	sfcName  = "sfc-test"
+	timeout   = 1 * time.Minute
+	interval  = 1 * time.Second
+	nfName    = "test-nf"
+	sfcName   = "sfc-test"
+	sfcNumMax = 12
 )
+
+func getMaxDpuResources(cli client.Client) int {
+	var nodeList corev1.NodeList
+	err := cli.List(context.TODO(), &nodeList)
+	Expect(err).NotTo(HaveOccurred())
+
+	var maxDPU resource.Quantity
+
+	for _, node := range nodeList.Items {
+		if val, ok := node.Status.Allocatable[corev1.ResourceName("openshift.io/dpu")]; ok {
+			if val.Cmp(maxDPU) > 0 {
+				maxDPU = val
+			}
+		}
+	}
+
+	v64 := maxDPU.Value()
+	v := int(v64)
+	Expect(int64(v)).To(Equal(v64))
+	return v
+}
+
+func sfcNew(i int, nfImage string) *configv1.ServiceFunctionChain {
+	return testutils.SfcNew(vars.Namespace,
+		fmt.Sprintf("%s%d", sfcName, i),
+		fmt.Sprintf("%s%d", nfName, i),
+		nfImage)
+}
+
+func sfcFindRunningSfc(c client.Client, sfcNum int, nfImage string) *configv1.ServiceFunctionChain {
+
+	var found_sfc *configv1.ServiceFunctionChain
+
+	Eventually(func() bool {
+		for i := 0; i < sfcNum; i++ {
+			sfc := sfcNew(i, nfImage)
+
+			pod := testutils.GetPod(c, sfc.Spec.NetworkFunctions[0].Name, sfc.ObjectMeta.Namespace)
+
+			if pod != nil && pod.Status.Phase == corev1.PodRunning {
+				found_sfc = sfc
+				break
+			}
+		}
+		return found_sfc != nil
+	}, timeout, 250*time.Millisecond).Should(BeTrue())
+
+	return found_sfc
+}
 
 func pingTest(srcClientSet kubernetes.Interface, srcRestConfig *rest.Config, srcPod *corev1.Pod, destIP, srcName, destName string) {
 	Eventually(func() bool {
@@ -109,6 +160,7 @@ var _ = g.AfterSuite(func() {
 })
 
 var _ = g.Describe("E2E integration testing", g.Ordered, func() {
+
 	var (
 		dpuSideClient                      client.Client
 		hostSideClient                     client.Client
@@ -320,20 +372,8 @@ var _ = g.Describe("E2E integration testing", g.Ordered, func() {
 		g.BeforeAll(func() {
 			imageRef = testutils.TrafficFlowTestsImage()
 
-			sfc = &configv1.ServiceFunctionChain{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      sfcName,
-					Namespace: vars.Namespace,
-				},
-				Spec: configv1.ServiceFunctionChainSpec{
-					NetworkFunctions: []configv1.NetworkFunction{
-						{
-							Name:  nfName,
-							Image: imageRef,
-						},
-					},
-				},
-			}
+			sfc = testutils.SfcNew(vars.Namespace, sfcName, nfName, imageRef)
+
 			nodeList, err := testutils.GetDPUNodes(hostSideClient)
 			Expect(err).NotTo(HaveOccurred())
 			pod := testutils.NewTestPod(testPodName, nodeList[0].Name)
@@ -463,6 +503,50 @@ var _ = g.Describe("E2E integration testing", g.Ordered, func() {
 				}, timeout, interval).Should(BeTrue())
 			})
 		})
+
+		g.Context("With more ServiceFunctionChains than resources", g.Ordered, func() {
+			g.It("Should have all SFC Pods eventually running", func() {
+
+				dpuResNum := getMaxDpuResources(dpuSideClient)
+				Expect(dpuResNum).To(BeNumerically(">=", 0))
+
+				sfcNum := (dpuResNum / 2) + 2
+				if sfcNum > sfcNumMax {
+					sfcNum = sfcNumMax
+				}
+
+				g.By(fmt.Sprintf("Creating %d SFCs to allocate all DPU side resources", sfcNum))
+				for i := 0; i < sfcNum; i++ {
+					sfc := sfcNew(i, imageRef)
+					testutils.SfcCreate(dpuSideClient, sfc)
+
+					Eventually(func() bool {
+						return testutils.GetPod(dpuSideClient, sfc.Spec.NetworkFunctions[0].Name, sfc.ObjectMeta.Namespace) != nil
+					}, timeout, interval).Should(BeTrue())
+				}
+
+				for i := 0; i < sfcNum; i++ {
+					g.By(fmt.Sprintf("Find a running SFC Pod (%d of %d)", i, sfcNum))
+					sfc := sfcFindRunningSfc(dpuSideClient, sfcNum, imageRef)
+
+					g.By(fmt.Sprintf("Delete SFC %s", sfc.ObjectMeta.Name))
+					err := dpuSideClient.Delete(context.TODO(), sfc)
+					Expect(err).NotTo(HaveOccurred())
+
+					Eventually(func() bool {
+						return testutils.GetPod(dpuSideClient, sfc.Spec.NetworkFunctions[0].Name, sfc.ObjectMeta.Namespace) == nil
+					}, timeout, interval).Should(BeTrue())
+				}
+
+				g.By(fmt.Sprintf("Check all %d SFCs are gone", sfcNum))
+				for i := 0; i < sfcNum; i++ {
+					sfc := sfcNew(i, imageRef)
+					sfc2 := testutils.SfcGet(dpuSideClient, sfc.ObjectMeta.Name, sfc.ObjectMeta.Namespace)
+					Expect(sfc2).To(BeNil())
+				}
+			})
+		})
+
 		g.AfterAll(func() {
 			// To maintain idempotency, make sure to clean up the SFC we created incase an earlier test failed
 			err := dpuSideClient.Delete(context.TODO(), sfc)
@@ -492,6 +576,10 @@ var _ = g.Describe("E2E integration testing", g.Ordered, func() {
 
 					return err != nil && errors.IsNotFound(err)
 				}, testutils.TestAPITimeout*30*2, testutils.TestRetryInterval).Should(BeTrue(), "Pod %s was not fully deleted in time", podName)
+			}
+
+			for i := 0; i < sfcNumMax; i++ {
+				dpuSideClient.Delete(context.TODO(), sfcNew(i, imageRef))
 			}
 		})
 	})
