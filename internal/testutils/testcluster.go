05f700ce7169de45738a2a32a2a3f07e6b444417
Balazs Nemeth <bnemeth@redhat.com>
2025-07-01 13:12:03 +0200

    refactor test helper functions

--- internal/testutils/testcluster.go
+++ internal/testutils/testcluster.go
@@ -13,12 +13,16 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/openshift/dpu-operator/pkgs/vars"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/remotecommand"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	configv1 "github.com/openshift/dpu-operator/api/v1"
 )
 
 type Cluster interface {
@@ -228,4 +232,69 @@ func GetFirstNode(c client.Client) (corev1.Node, error) {
 		return corev1.Node{}, fmt.Errorf("No nodes found in cluster")
 	}
 	return nodes.Items[0], nil
+}
+
+func DpuOperatorNamespace() *corev1.Namespace {
+	namespace := &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: vars.Namespace,
+		},
+		Spec:   corev1.NamespaceSpec{},
+		Status: corev1.NamespaceStatus{},
+	}
+	return namespace
+}
+
+func DpuOperatorCR(name string, mode string, ns *corev1.Namespace) *configv1.DpuOperatorConfig {
+	config := &configv1.DpuOperatorConfig{}
+	config.SetNamespace(ns.Name)
+	config.SetName(name)
+	config.Spec = configv1.DpuOperatorConfigSpec{
+		Mode:     mode,
+		LogLevel: 2,
+	}
+	return config
+}
+
+func CreateNamespace(client client.Client, ns *corev1.Namespace) {
+	// ignore error when creating the namespace since it can already exist
+	client.Create(context.Background(), ns)
+	found := corev1.Namespace{}
+	Eventually(func() error {
+		return client.Get(context.Background(), types.NamespacedName{Namespace: vars.Namespace, Name: ns.GetName()}, &found)
+	}, TestAPITimeout, TestRetryInterval).Should(Succeed())
+}
+
+func DeleteNamespace(client client.Client, ns *corev1.Namespace) {
+	client.Delete(context.Background(), ns)
+	found := corev1.Namespace{}
+	Eventually(func() error {
+		err := client.Get(context.Background(), types.NamespacedName{Namespace: vars.Namespace, Name: ns.GetName()}, &found)
+		if errors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}, TestAPITimeout, TestRetryInterval).Should(Succeed())
+}
+
+func CreateDpuOperatorCR(client client.Client, cr *configv1.DpuOperatorConfig) {
+	err := client.Create(context.Background(), cr)
+	Expect(err).NotTo(HaveOccurred())
+	found := configv1.DpuOperatorConfig{}
+	Eventually(func() error {
+		return client.Get(context.Background(), types.NamespacedName{Namespace: cr.GetNamespace(), Name: cr.GetName()}, &found)
+	}, TestAPITimeout, TestRetryInterval).Should(Succeed())
+}
+
+func DeleteDpuOperatorCR(client client.Client, cr *configv1.DpuOperatorConfig) {
+	client.Delete(context.Background(), cr)
+	found := configv1.DpuOperatorConfig{}
+	Eventually(func() error {
+		err := client.Get(context.Background(), types.NamespacedName{Namespace: vars.Namespace, Name: cr.GetName()}, &found)
+		if errors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}, TestAPITimeout, TestRetryInterval).Should(Succeed())
 }
