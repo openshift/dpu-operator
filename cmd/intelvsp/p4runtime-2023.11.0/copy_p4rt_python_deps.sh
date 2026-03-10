7c4c25073114c8204950790495c5e7dc056e7c26
LuLo <flex.muscle.0t@icloud.com>
2026-03-10 10:00:16 +0100

    fix(intelvsp): ensure python3 usage and clean whitespace in p4rt deps script
    Updated copy_p4rt_python_deps.sh to use pip3 explicitly instead of pip to avoid
    potential Python 2/3 compatibility issues. Also cleaned up trailing whitespace
    from errexit option setting.

--- cmd/intelvsp/p4runtime-2023.11.0/copy_p4rt_python_deps.sh
+++ cmd/intelvsp/p4runtime-2023.11.0/copy_p4rt_python_deps.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -eux
 #Note, set pipefail supported in bash, but not in sh, so below is the alternative to check failures when using pipe(with /bin/sh)
-set -o errexit 
+set -o errexit
 
-output="$(pip show pip | grep Location)" && \
+output="$(pip3 show pip | grep Location)" && \
 pkg_install_path="$(echo "$output" | awk '{print $2}')" && \
 echo "$pkg_install_path" && \
 mkdir -p $pkg_install_path/p4 && \
