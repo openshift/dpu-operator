6d4f47320b9fe55a05dc2831db5e762ee56ab8e6
Sam DaSilva <sam.ramos.dasilva@gmail.com>
2026-03-23 14:32:11 -0400

    fix(vsp): make p4 hostPath writable
    Signed-off-by: Sam DaSilva <sam.ramos.dasilva@gmail.com>

---
+++ internal/utils/labels.go
@@ -0,0 +1,13 @@
+package utils
+
+const (
+	DpuSideLabelKey = "dpu.config.openshift.io/dpuside"
+
+	DpuSideLabelValueDpu  = "dpu"
+	DpuSideLabelValueHost = "dpu-host"
+
+	P4HostPathLabelKey = "dpu.config.openshift.io/p4hostpath"
+
+	P4HostPathLabelValueOpt    = "opt"
+	P4HostPathLabelValueVarOpt = "varopt"
+)
